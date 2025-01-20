c38433e53b725ccebf67995c96de0bb6c6c60ce5
Cledersonbc <cledersonbc@live.com>
2025-01-20 03:45:57 -0300

    added simple upload

--- vapi/routes.py
+++ vapi/routes.py
@@ -1,5 +1,30 @@
 from vapi import app
+from flask import request
+import ffmpeg
+from flask import send_file
+import shutil
+import os
 
 @app.route('/')
 def running():
     return 'Server is running'
+
+@app.route('/download/zip', methods=['POST'])
+def download():
+    if 'file' not in request.files:
+        return 'Arquivo não enviado'
+    
+    file = request.files['file']
+    print(file)
+    filename = os.path.join('/tmp', 'api', 'video', 'vimg-video.mp4')
+    output = os.path.join('/tmp', 'api', 'imgs', 'vimg-%d.png')
+    file.save(filename)
+    file.close()
+
+    (
+	ffmpeg.input(filename)
+	    .output(output)
+	    .run()
+    )
+    shutil.make_archive('/tmp/api/compactado', 'zip', '/tmp/api/imgs')
+    return send_file('/tmp/api/compactado.zip', as_attachment=True)
